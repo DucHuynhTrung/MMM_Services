0b4d07a5270bb4a4908f8b6a3a5e97359d82f72e
Huỳnh Trung Đức <huynhtrungduc.growth@gmail.com>
2025-02-17 08:27:54 +0000

    thử nghiệm xử lý lệnh và thu nhập thông tin đầu vào cần thiết cho lệnh xử lý

--- app/services/telegram_service.py
+++ app/services/telegram_service.py
@@ -3,18 +3,55 @@ import os, telebot
 TELEGRAM_BOT = telebot.TeleBot(os.environ['TELEGRAM_BOT_TOKEN'])
 
 
+@TELEGRAM_BOT.message_handler(commands=['start'])
+def command_start(message):
+  chat_id = message.chat.id
+  TELEGRAM_BOT.send_message(chat_id, "Hi!")
+
+
+@TELEGRAM_BOT.message_handler(commands=['help'])
+def command_help(message):
+  chat_id = message.chat.id
+  TELEGRAM_BOT.send_message(chat_id, "Help!")
+
+
+@TELEGRAM_BOT.message_handler(commands=['chi'])
+def command_help(message):
+  chat_id = message.chat.id
+  TELEGRAM_BOT.send_message(chat_id, "Help!")
+
+
 @TELEGRAM_BOT.message_handler(func=lambda message: True)
 def all_message(message):
   chat_id = message.chat.id
   user_message = message.text
 
-  # data_transaction = handle_message_transaction(user_message)
+  if user_message == "/chi":
+    ask = TELEGRAM_BOT.send_message(chat_id,
+                                    "Được rồi bạn đã chi tiêu gì nào?")
+    TELEGRAM_BOT.register_next_step_handler(ask, handle_message_transaction)
 
-  response_message = f"Received: {user_message}"
-  TELEGRAM_BOT.send_message(chat_id, response_message)
+
+def handle_message_transaction(message: str):
+  data: object = {}
+  if message == "/chi":
+    data = handle_expenditure(message)
+  elif message == "/thu":
+    data = handle_income(message)
+
+  if data != {}:
+    handle_database(data)
 
 
-def handle_message_transaction(message: str) -> object:
+def handle_expenditure(message: str) -> object:
+  pass
+
+
+def handle_income(message: str) -> object:
+  pass
+
+
+def handle_database(data: object):
   pass
 
 
