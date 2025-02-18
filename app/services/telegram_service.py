61d0df7152392451ea48ce2b7bda098ec2b0d9b8
Huỳnh Trung Đức <huynhtrungduc.growth@gmail.com>
2025-02-18 10:17:35 +0000

    Test thử command vs next step response

--- app/services/telegram_service.py
+++ app/services/telegram_service.py
@@ -1,4 +1,5 @@
 import os, telebot
+from ..utils import handle_message_income, handle_message_expenditure, handle_message_credit
 
 TELEGRAM_BOT = telebot.TeleBot(os.environ['TELEGRAM_BOT_TOKEN'])
 
@@ -6,19 +7,37 @@ TELEGRAM_BOT = telebot.TeleBot(os.environ['TELEGRAM_BOT_TOKEN'])
 @TELEGRAM_BOT.message_handler(commands=['start'])
 def command_start(message):
   chat_id = message.chat.id
-  TELEGRAM_BOT.send_message(chat_id, "Hi!")
-
-
-@TELEGRAM_BOT.message_handler(commands=['help'])
-def command_help(message):
-  chat_id = message.chat.id
-  TELEGRAM_BOT.send_message(chat_id, "Help!")
+  first_name = message.chat.first_name
+  TELEGRAM_BOT.send_message(
+      chat_id, f"Hi {first_name}, Tôi là bot hỗ trợ quản lý cho bạn.")
+  ask_cash = TELEGRAM_BOT.send_message(
+      chat_id,
+      f"Hãy cho tôi biết tổng tiền bạn đang có theo cấu trúc sau: \n'tiền mặt, tiền ngân hàng (tất cả ngân hàng), hạn mức tín dụng, số tiền tín dụng khả dụng, số tiền nợ"
+  )
+  TELEGRAM_BOT.register_next_step_handler(ask_cash, handle_start)
 
 
 @TELEGRAM_BOT.message_handler(commands=['chi'])
-def command_help(message):
+def command_chi(message):
   chat_id = message.chat.id
-  TELEGRAM_BOT.send_message(chat_id, "Help!")
+
+  ask = TELEGRAM_BOT.send_message(chat_id, "Được rồi bạn đã chi tiêu gì nào?")
+  TELEGRAM_BOT.register_next_step_handler(ask, handle_expenditure)
+
+
+@TELEGRAM_BOT.message_handler(commands=['thu'])
+def command_thu(message):
+  pass
+
+
+@TELEGRAM_BOT.message_handler(commands=['tindung'])
+def command_tindung(message):
+  pass
+
+
+@TELEGRAM_BOT.message_handler(commands=['tratindung'])
+def command_tratindung(message):
+  pass
 
 
 @TELEGRAM_BOT.message_handler(func=lambda message: True)
@@ -26,28 +45,39 @@ def all_message(message):
   chat_id = message.chat.id
   user_message = message.text
 
-  if user_message == "/chi":
-    ask = TELEGRAM_BOT.send_message(chat_id,
-                                    "Được rồi bạn đã chi tiêu gì nào?")
-    TELEGRAM_BOT.register_next_step_handler(ask, handle_message_transaction)
+  TELEGRAM_BOT.send_message(chat_id, user_message)
 
 
-def handle_message_transaction(message: str):
-  data: object = {}
-  if message == "/chi":
-    data = handle_expenditure(message)
-  elif message == "/thu":
-    data = handle_income(message)
+def handle_start(message):
+  chat_id = message.chat.id
+  cash, bank, credit, available_credit, debt = [0, 0, 0, 0, 0]
+  try:
+    cash, bank, credit, available_credit, debt = [
+        float(i) for i in message.text.split(", ")
+    ]
 
-  if data != {}:
+    data: object = {
+        type: "start",
+        cash: cash,
+        bank: bank,
+        credit: credit,
+        available_credit: available_credit,
+        debt: debt
+    }
     handle_database(data)
 
+  except:
+    TELEGRAM_BOT.send_message(
+        chat_id,
+        "Error 101: Quá trình phần tích đã xảy ra lỗi. Vui lòng thử lại bằng cách bấm lệnh /start"
+    )
 
-def handle_expenditure(message: str) -> object:
+
+def handle_expenditure(message: str):
   pass
 
 
-def handle_income(message: str) -> object:
+def handle_income(message: str):
   pass
 
 
---
+++ app/utils/telegram_helper.py
@@ -0,0 +1,10 @@
+def handle_message_expenditure(message: str) -> object:
+  pass
+
+
+def handle_message_income(message: str) -> object:
+  pass
+
+
+def handle_message_credit(message: str) -> object:
+  pass
